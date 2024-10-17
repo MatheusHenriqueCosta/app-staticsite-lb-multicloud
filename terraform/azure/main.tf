fe6f88bb2fddcaa638606973bca7a8f62dfce6cf
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-16 22:31:07 -0300

    testando coisa

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -299,7 +299,7 @@ resource "azurerm_network_interface_backend_address_pool_association" "vm01" {
     network_interface_id    = azurerm_network_interface.vm01.id
     backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
 }
-
+#
 resource "azurerm_network_interface_backend_address_pool_association" "vm02" {
     ip_configuration_name   = "vm02"
     network_interface_id    = azurerm_network_interface.vm02.id
