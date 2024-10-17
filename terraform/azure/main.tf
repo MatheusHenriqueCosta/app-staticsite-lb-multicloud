9a63e3ec334c915868f0f630a15fab277904509b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-16 21:32:21 -0300

    Testando criacao

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-staticsite-lb-multicloud-tf-matheusg"
+    name     = "rg-staticsite-lb-multicloud-tf-matheusg123"
     location = "brazilsouth"
 }
 
